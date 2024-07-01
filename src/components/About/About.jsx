bbdd743d663c6e9440542a21fc7bb76efabbcfb1
kobiowuquadri <kobiowuq@gmail.com>
2024-07-01 18:48:43 +0100

    update: about me section content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { FaCode, FaLaptopCode, FaUserGraduate } from 'react-icons/fa';
 import profileImg from '../../assets/20240613_105504.jpg'
-
+import myCV from '../../assets/QUADRI_ KOBIOWU_ CV.pdf'
 
 const AboutMe = () => {
   return (
@@ -24,7 +24,7 @@ const AboutMe = () => {
           >
             <img 
               src={profileImg}
-              alt="Your Name"
+              alt="Quadri Kobiowu"
               className="rounded-lg shadow-lg w-full"
             />
           </motion.div>
@@ -34,10 +34,16 @@ const AboutMe = () => {
             transition={{ duration: 0.5, delay: 0.4 }}
           >
             <p className="text-lg mb-6 font-poppins">
-              Hello! I am Quadri Kobiowu, a passionate fullstack developer with a keen interest in creating efficient, scalable, and user-friendly web applications. With 3 years of experience in the field, I have had the opportunity to work on a diverse range of projects, from small business websites to complex enterprise applications.
+              Hello! I am Quadri Kobiowu, a Backend Engineer at <a target='_blank' style={{color: 'blue'}} href="https://zealightlabs.com/">Zealight Innovation Labs</a> and a passionate fullstack developer with a keen interest in creating efficient, scalable, and user-friendly web applications. With 3 years of experience in the field, I have had the opportunity to work on a diverse range of projects, from small business websites to complex enterprise applications.
             </p>
             <p className="text-lg mb-6 font-poppins">
-              My journey in tech began 2020. Since then, I have been constantly learning and adapting to new technologies and methodologies in the ever-evolving world of web development.
+              My journey in tech began in 2020. Since then, I have been constantly learning and adapting to new technologies and methodologies in the ever-evolving world of web development.
+            </p>
+            <p className="text-lg mb-6 font-poppins">
+              Coming from an average background family with little hope for the future, learning to code changed my life for the better. I'm truly grateful for the opportunities it has provided me, and I'm passionate about helping children from poor families transform their lives through coding and technology.
+            </p>
+            <p className="text-lg mb-6 font-poppins">
+             Thank you.
             </p>
             <div className="grid grid-cols-3 gap-4 mb-6">
               {[
@@ -58,9 +64,9 @@ const AboutMe = () => {
             <motion.button
               whileHover={{ scale: 1.05, backgroundColor: '#333' }}
               whileTap={{ scale: 0.95 }}
-              className="bg-black text-white font-bold py-2 px-4 rounded-full inline-flex items-center font-poppins transition-colors duration-300"
+              className="bg-black text-white font-bold py-2 px-4 rounded-full inline-flex items-center font-poppins transition-colors duration:300"
             >
-              Download CV
+              <a href={myCV} target='_blank'>Download CV</a>
             </motion.button>
           </motion.div>
         </div>
